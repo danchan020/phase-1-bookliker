ca62ab7f5639ef97eefe995886acd856d128be32
Daniel <danchan020@gmail.com>
2022-05-03 10:16:42 -0700

    Commit 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+const url = 'http://localhost:3000/books'
+
+function getBook(urls){
+    fetch(urls).then(resp => resp.json()).then(data => {
+        function shallowIterator(data) {
+            for (const key in data) {
+            console.log(data[key]);
+    }
+
+
+
+      //         => {
+// for (const info in data){
+//         let title = data.title
+//                 console.log(title)
+// }
+//     })
